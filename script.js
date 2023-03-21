e8dfe45101499557a874265da19e5fae8f1e0649
Dbeee-Acharya <dbeee.acharya@gmail.com>
2023-03-21 13:54:32 +0545

    Create a function that randomly returns rock, paper or scissors

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 //main Script for rock paper scissors game
 
-console.log("Test")
+/* 
+    TODO
+    1. take userInput (string)
+    2. Create a function that randomly generates rock paper or scissors
+    3. Create a function to compare userInput with Random result and declare winner
+*/
+
+//take userInput 
+
+
+function getComputerSelection() {
+    let number = Math.floor(Math.random() * 3); // generates a random number between from 0 to 2
+
+    if (number === 0) {
+        return "rock";
+    } else if (number === 1) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+let list =[]
+for (let i = 0; i <= 10; i++) {
+    list.push(getComputerSelection());
+}
+console.log(list);
+
+//compare 
