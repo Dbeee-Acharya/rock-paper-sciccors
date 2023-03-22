3ba340b0eb26f27c32410a26f27f3ac4adfa0930
Dbeee-Acharya <dbeee.acharya@gmail.com>
2023-03-22 18:33:11 +0545

    Add function to decide the winner
    this function takes two parameters and checks for the winner. It returns
    different strings based on who won.

--- script.js
+++ script.js
@@ -7,10 +7,12 @@
     3. Create a function to compare userInput with Random result and declare winner
 */
 
-//take userInput 
+
+
 function userInput(){
-    let choice = (prompt("rock, paper or scissors ?")).toLowerCase();
-    return choice;
+    // let choice = (prompt("rock, paper or scissors ?")).toLowerCase();
+    // return choice;
+    return "rock";
 };
 
 //this function returns either rock, paper or scissors 
@@ -22,7 +24,25 @@ function getComputerSelection() {
     return choices[number];
 };
 
-//compare 
-function compareSelection(userInput, getComputerSelection) {
+function compareSelection(user, computer) {
+    let userWin = 0;
+    let computerWin = 0;
+
     
+    if (user() === computer()) {
+        return "Its a Draw"
+    }
+
+    if ((user() === "rock" && computer() === "scissors") || 
+        (user() === "paper" && computer() === "rock") ||
+        (user() === "scissors" && computer() === "paper")) {
+            userWin += 1
+            return "User wins this round"
+    } else {
+        computerWin += 1
+        return "Computer Wins this round"
+    }
 };
+
+console.log(getComputerSelection());
+console.log(compareSelection(userInput, getComputerSelection));
